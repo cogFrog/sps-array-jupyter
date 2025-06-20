2a58aa2d2f77ec56256c9d922d6a0a7ba8508dae
cogFrog <skyebedard@gmail.com>
2025-06-19 22:27:08 -0500

    getting helper classes worked out, AF calculation still in progress

--- antennasAndArrays.py
+++ antennasAndArrays.py
@@ -14,20 +14,34 @@ class AntennaArray:
 
     def __repr__(self):
         return str(self.antennas)
+    
+
+    def get_pattern(self, azimuth, elevation):
+        # TODO all of this is completely wrong, fix it
+        azimuth_grid, elevation_grid = np.meshgrid(azimuth, elevation)
+        u_grid = np.sin(azimuth_grid / 180 * np.pi)
+        v_grid = np.sin(elevation_grid/180*np.pi)
+
+        AF = np.zeros(np.shape(u_grid), dtype=complex)
+
+        for antenna in self.antennas:
+            AF = AF + np.exp(-1j * 2 * np.pi * (antenna.x()*u_grid + antenna.y()*v_grid))
+
+        return np.transpose(AF)
 
 
 class rectangularArray(AntennaArray):
     def __init__(self, x, y, spacing):
-        self.array = AntennaArray()
+        super().__init__()
+
         for i in range(x):
             for j in range(y):
-                self.array.append(Antenna([i*spacing, j*spacing, 0]))
-
-        print(self.array)
+                self.antennas.append(Antenna([i*spacing, j*spacing, 0]))
 
 
 class Antenna:
     # TODO add antenna radiation patterns
+    # position defaults to value in terms of wavelength
     def __init__(self, position):
         # TODO type checking
         self.pos = np.array(position[0:3])
@@ -43,3 +57,6 @@ class Antenna:
     
     def __repr__(self):
         return f"Antenna ({self.pos[0]}, {self.pos[1]}, {self.pos[2]})"
+
+ar = rectangularArray(2, 2, 0.5)
+print(10*np.log10(np.abs(ar.get_pattern(np.linspace(0, np.pi, num=50), (0)))))
