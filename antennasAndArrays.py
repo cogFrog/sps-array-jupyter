39718ec39b2ffcc6bea707e2389f6de5a9ba50be
cogFrog <skyebedard@gmail.com>
2025-07-16 23:32:16 -0500

    starting proper refactor, using numpy properly

--- antennasAndArrays.py
+++ antennasAndArrays.py
@@ -1,23 +1,66 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
 class AntennaArray:
-    def __init__(self, *args):
+    """Basic array of identical antennas, minimal assumptions
+    """    
+    def __init__(self, positions, excitations=None):
+        """Initialize antenna array with N antenna positions and excitations
 
-        if len(args) == 0:
-            self.antennas = []
-        elif len(args) == 1:
-            # TODO type checking
-            self.antennas = antennaList
+        Args:
+            positions (nparray(N, 3)): 2D array representing position of all antennas in wavelengths. Each row is an antenna position [x, y, z]
+            excitations (nparray(N)): Array of complex #s representing excitation of each antenna. If None, defaults to all 1's
+        """
+
+        if len(positions.shape) != 2:
+            raise ValueError(f'Expected 2D nparray, found {len(positions.shape)}D nparray')
+        if positions.shape[1] != 3:
+            raise ValueError(f'Expected 3 column nparray, found {positions.shape[1]} columns')
+        self.positions = positions
+
+        if excitations is None:
+            excitations = np.ones(positions.shape[0])
+        elif positions.shape[0] != excitations.shape[0]:
+            raise ValueError(f'Expected positions and excitations to have same number of rows (antennas), found {positions.shape[0]} and {excitations.shape[0]} rows')
+        self.excitations = excitations
+
+        # TODO add an antenna object that can describe the antenna properties such as antenna rad pattern?
     
-    def append(self, antenna):
-        self.antennas.append(antenna)
+    @classmethod
+    def empty(self):
+        """Initialize an empty antenna array
+        """
+        return self(np.array((0,3)))
+    
+    def append(self, position, excitation=None):
+        """Append a single antenna's position and excitation
+
+        Args:
+            position (nparray(n,3)): Array represention position [x, y, z] in wavelengths (can be any number of rows)
+            excitation (nparray, optional): Complex number representing the excitation of the antenna. Defaults to None.
+        """
+        self.positions = np.vstack((self.positions, position))
+
+        # TODO update this so it can handle appending multiple antennas at the same time
+        if excitation is None:
+            excitation = np.array([1])
+        self.excitations = np.hstack((self.excitations, np.array(excitation)))
 
     def __repr__(self):
         return str(self.antennas)
-    
 
-    def getPattern(self, azimuth, elevation):
+    def arrayFactor(self, azimuth, elevation):
+        """The most general form of the array factor calculation. No assumptions about the array or
+           the range of azimuth/elevations is made.
+
+           AF()
+
+        Args:
+            azimuth (float): _description_
+            elevation (float): _description_
+
+        Returns:
+            nparray: _description_
+        """
         # TODO all of this is completely wrong, fix it
         azimuth_grid, elevation_grid = np.meshgrid(azimuth, elevation)
         u_grid = np.sin(azimuth_grid / 180 * np.pi)
@@ -30,31 +73,4 @@ class AntennaArray:
 
         return np.transpose(AF)
 
-
-class rectangularArray(AntennaArray):
-    def __init__(self, x, y, spacing):
-        super().__init__()
-
-        for i in range(x):
-            for j in range(y):
-                self.antennas.append(Antenna([i*spacing, j*spacing, 0]))
-
-
-class Antenna:
-    # TODO add antenna radiation patterns
-    # position defaults to value in terms of wavelength
-    def __init__(self, position):
-        # TODO type checking
-        self.pos = np.array(position[0:3])
-    
-    def x(self):
-        return self.pos[0]
-    
-    def y(self):
-        return self.pos[1]
-    
-    def z(self):
-        return self.pos[2]
-    
-    def __repr__(self):
-        return f"Antenna ({self.pos[0]}, {self.pos[1]}, {self.pos[2]})"
+# TODO add more array variants (rectanglular array, circular array, sparce arrays, different simplifications)
